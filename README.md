57343dbae2af43a83694b381ce4ce9fb616a166a
Peter <ayenicology@gmail.com>
2019-10-18 11:57:27 +0100

    Changed packaged to angular 7

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NgEventApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.9.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.2.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ng-event-app');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-event-app app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-event-app!');
   });
 });
